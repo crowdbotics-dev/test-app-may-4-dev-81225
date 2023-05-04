98674954237bd2a21ba2598d26b116b4bdd049a1
Mike Young <mike.young@crowdbotics.com>
2023-05-04 11:33:24 -0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,7 +1,8 @@
 export const globalOptions = {
-  name: "test-app-may-4-dev-81225",
-  url: "https://test-app-may-4-dev-81225.botics.co",
-  api: "https://test-app-may-4-dev-81225.botics.co/api/v1"
+  "name": "test-app-may-4-dev-81225",
+  "url": "https://test-app-may-4-dev-81225.botics.co",
+  "api": "https://test-app-may-4-dev-81225.botics.co/api/v1",
+  "initialRoute": "Untitled1"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
